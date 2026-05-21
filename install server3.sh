32293a1a65c2a3b01cd28e23b648182ea9f4ea06
SHIVAM GARUD <144516473+Shivamgarud8@users.noreply.github.com>
2026-05-21 13:25:21 +0530

    Create install server3.sh

---
+++ install server3.sh
@@ -0,0 +1,7 @@
+sudo apt update
+
+sudo apt install -y adduser libfontconfig1 musl
+
+wget https://dl.grafana.com/enterprise/release/grafana-enterprise_12.1.0_amd64.deb
+
+sudo dpkg -i grafana-enterprise_12.1.0_amd64.deb
